c24cd7a4b4d3987ac6cabdcc9f3ceee5b93425a0
Christiane Girao <christianegirao@gmail.com>
2017-12-01 17:15:30 -0300

    primeira versao

---
+++ src/main/java/com/cgac/pastoralSocial/PastoralSocialApplication.java
@@ -0,0 +1,12 @@
+package com.cgac.pastoralSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastoralSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastoralSocialApplication.class, args);
+	}
+}
